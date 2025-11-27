e973840b8d9a6f0fcdad82b2d92bab568703bdfa
lee <xlegendman@gmail.com>
2025-11-27 00:02:30 -0500

    change font and add lazyness :)

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp ~/.gtkrc-2.0 ~/Documents/GitHub/dotfiles/
+cp -r ~/.config/gtk-3.0 ~/Documents/GitHub/dotfiles/
+cp -r ~/.config/gtk-4.0 ~/Documents/GitHub/dotfiles/
+
+cp -r ~/.config/kitty ~/Documents/GitHub/dotfiles/
+cp -r ~/.config/hypr ~/Documents/GitHub/dotfiles/
+cp -r ~/.config/waybar ~/Documents/GitHub/dotfiles/
+cp -r ~/.config/micro ~/Documents/GitHub/dotfiles/
+cp -r ~/.config/rofi ~/Documents/GitHub/dotfiles/
+cp -r ~/.config/fish ~/Documents/GitHub/dotfiles/
+cp -r ~/.config/fastfetch ~/Documents/GitHub/dotfiles/
